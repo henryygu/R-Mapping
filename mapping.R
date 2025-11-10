2cf143428c712504aa6bff77ece5136713d1d406
henryygu <32582668+henryygu@users.noreply.github.com>
2025-11-10 15:02:49 +0000

    initial

---
+++ mapping.R
@@ -0,0 +1,20 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(sf,dplyr,ggplot2)
+
+
+
+# ---- 1. Read the shapefile ----
+# The shapefile folder should contain files like .shp, .dbf, .shx, etc.
+shp <- st_read("shapefile")
+
+# ---- 2. Read the CSV file ----
+book1 <- read.csv("book1.csv", stringsAsFactors = FALSE)
+
+# ---- 3. Join the data ----
+joined <- shp %>%
+  left_join(book1, by = c("ICB23CD" = "ICB24CD"))
+
+# ---- 4. Plot the map ----
+ggplot(joined) +
+  geom_sf(aes(fill = Percentage_in_4_hours_or_less_.all.), color = "white", size = 0.2) +
+  theme_minimal()
